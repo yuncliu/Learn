36a3794f82e6eac7c55882ac6016c661461be308
yuncliu <lyc1990@qq.com>
2016-03-30 21:50:39 +0800

    improvement

--- python/http_tool.py
+++ python/http_tool.py
@@ -4,6 +4,7 @@ import socket
 import os
 import sys
 import http.client
+from http import HTTPStatus
 import argparse
 import ssl
 
@@ -17,12 +18,23 @@ class Request(object):
         self.sni = None
 
     def set_addr(self, addr):
-        self.host, self.port = addr.split(':')
-        if self.port.isnumeric():
-            self.port = int(self.port)
+        """
+        addr    address to send.
+                valid addr:
+                    localhost:80
+                    127.0.0.1:80
+                    localhost
+                    127.0.0.1
+        """
+        if addr.find(':') >=0:
+            self.host, self.port = addr.split(':')
+            if self.port.isnumeric():
+                self.port = int(self.port)
+            else:
+                print('port is not number')
+                raise Exception('Port should be a number')
         else:
-            print('port is not number')
-            raise Exception('Port should be a number')
+            self.host = addr
 
     def get_addr(self):
         return self.host, self.port
@@ -31,20 +43,25 @@ class Request(object):
         return self._text
 
     def set_text(self, t):
+        """
+        set text to send
+        """
         self._text = t
         self.req = self._text.replace(os.linesep, '')
         self.req = self.req.replace('\\r', '\r')
         self.req = self.req.replace('\\n', '\n')
 
     def send(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         if self.use_https:
-            context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
-            if self.sni is None:
-                s = context.wrap_socket(socket.socket(socket.AF_INET), server_hostname=self.host)
-            else:
-                s = context.wrap_socket(socket.socket(socket.AF_INET), server_hostname=self.sni)
-        else:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            ctx = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+            s = ctx.wrap_socket(socket.socket(socket.AF_INET),
+                    server_hostname = self.host if self.sni is None else self.sni)
+
+        if  self.port is None:
+            self.port = 80
+            if self.use_https:
+                self.port = 443
 
         # now connect to the web server on port 80 - the normal http port
         try:
@@ -64,43 +81,40 @@ class Request(object):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--addr', default='localhost:90', help="set address --addr=localhost:80")
-    parser.add_argument('--https', action='store_true', default = False, help="if use https")
+    parser.add_argument('--addr', default='localhost', help="""set address default is 
+            --addr=localhost:<port> 80 for http, 443 for https""")
+    parser.add_argument('--https', action='store_true', default = False, help="if use https, only TLSv1.2")
     parser.add_argument('--sni', help="set Server Name Indication")
     parser.add_argument('--file', help="set file to send")
+    parser.add_argument('--printbody', action='store_true', default = False, help="if print body of response")
     args = parser.parse_args()
 
     if len(sys.argv) == 1:
-        #parser.print_help()
-        parser.print_usage()
+        parser.print_help()
         return
 
     x = Request()
-
-    if args.addr:
-        x.addr = args.addr
-    else:
-        x.addr = 'localhost:80'
-
-    if args.https:
-        print('https enabled')
-        x.use_https = True
-        x.port = 443
-    else:
-        print('https not enabled')
-
-    if args.file:
-        x.text = open(args.file, 'r').read()
-    else:
-        x.text = 'GET / HTTP/1.1\\r\\n\\r\\n'
+    x.addr = args.addr
+    x.sni = args.sni # Server Name Indication
+    x.use_https = args.https #if use https
+    x.text = open(args.file, 'r').read() if args.file else 'GET / HTTP/1.1\\r\\n\\r\\n'
 
     resp  = x.send()
     if resp is None:
         print('no response')
         return
-    print('Response [{0}]:'.format(resp.code))
-    for i in resp.getheaders():
-        print(i)
+    print('>>> Status Code = [{0}] info = [{1}]'.format(resp.code, HTTPStatus(resp.code).description))
+    for k, v in resp.getheaders():
+        print('{0}:{1}'.format(k, v))
+
+    if args.printbody:
+        length = resp.getheader('content-length')
+        if length is not None:
+            print('>>> Body length is {0}'.format(length))
+            body = resp.read(int(length)).decode()
+            print(body)
+        else:
+            print("Can't get content-length from header")
 
 if __name__ == "__main__":
     main()
