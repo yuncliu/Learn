f274ef885bbc5dd707fedb55aa851c5b326459e7
yuncliu <lyc1990@qq.com>
2016-05-30 21:53:21 -0400

    simple server.js completed

--- react/server.js
+++ react/server.js
@@ -1,12 +1,24 @@
 let http = require("http");
 let url = require("url");
 let fs = require("fs");
-let path=require('path');
+let path=require("path");
 
 function handleDirectory(p, response) {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("directory");
-    response.end();
+    fs.readdir(p,{flag : "r", encoding:"utf8"}, (err, files)=>{
+        if (err) {
+            response.writeHead(404, {"Content-Type": "text/html"});
+            response.end();
+        } else {
+            response.writeHead(200, {"Content-Type": "text/html"});
+            response.write("<ul>");
+            for (let file of files) {
+                let s = "<li><a href=" +file + ">" + file + "</a></li>"
+                response.write(s);
+            }
+            response.write("</ul>");
+            response.end();
+        }
+    });
 }
 
 function onRequest(request, response) {
@@ -17,21 +29,43 @@ function onRequest(request, response) {
     fs.stat(p, (err, stats)=>{
         console.log("process [%s]", p);
         if (err) {
-            console.log('%s not exist', p);
+            console.log("%s not exist", p);
             response.writeHead(404, {"Content-Type": "text/html"});
             response.end();
         } else {
             if(stats.isFile()) {
-                console.log('%s is a file', p);
-                //ex = path.extname(p)
+                console.log("%s is a file", p);
+            }
+        }
+    });
+}
+
+function onRequest(request, response) {
+    let pathname = url.parse(request.url).pathname;
+    let p = path.join(__dirname, pathname);
+    console.log(p);
+    fs.stat(p, (err, stats)=>{
+        console.log("process [%s]", p);
+        if (err) {
+            console.log("%s not exist", p);
+            response.writeHead(404, {"Content-Type": "text/html"});
+            response.end();
+        } else {
+            if(stats.isFile()) {
+                console.log("%s is a file", p);
+                ex = path.extname(p)
                 fs.readFile(p, {flag:"r", encoding:"utf8"}, (err, data)=>{
-                    response.writeHead(200, {"Content-Type": "text/html"});
+                    if (ex==".html") {
+                        response.writeHead(200, {"Content-Type": "text/html"});
+                    } else {
+                        response.writeHead(200, {"Content-Type": "text/plain"});
+                    }
                     response.write(data);
                     response.end();
                 });
             }
             if (stats.isDirectory()) {
-                console.log('%s is a directory', p);
+                console.log("%s is a directory", p);
                 handleDirectory(p, response)
             }
         }
