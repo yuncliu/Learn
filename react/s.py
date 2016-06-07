09204820bdd181f35ed610277491fd94667fe982
yuncliu <lyc1990@qq.com>
2016-06-07 00:16:24 -0400

    modify

--- react/s.py
+++ react/s.py
@@ -1,29 +1,30 @@
-from flask import Flask
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import os
+from flask import Flask, render_template, abort
 
-app = Flask(__name__, static_url_path='', static_folder='')
+app = Flask(__name__)
 
+@app.route('/api/<api>')
+def api(api):
+    return "Hellow"
 
-def serve_dir(path):
-    h = "<html>\n"
-    h = h + "<ul>\n"
-    for entry in os.scandir(path):
-        a = "<li><a href='{0}/{1}'>{1}</a></li>\n".format(path, entry.name)
-        h = h + a
+@app.route('/', defaults={'path': '.'})
+@app.route('/<path:path>')
+def root(path):
+    print('[{0}]'.format(path))
+    print(os.getcwd())
+    if os.path.isdir(path):
+        data = [os.path.join(path, entry.name) for entry in os.scandir(path)]
+        for i in data:
+            print(i)
+        return render_template('index.html', data=data)
 
-    h = h + "</ul>\n"
-    h = h + "</html>"
-    return h
+    if os.path.isfile(path):
+        with open(path) as f:
+            return f.read()
 
-@app.route('/')
-def root():
-    print('request path is /')
-    return serve_dir('.')
+    abort(404)
 
-@app.route('/api/<path>')
-def path(path):
-    print(path)
-    return "api"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000)
+if __name__=="__main__":
+    app.run(host='0.0.0.0', port=8080, debug=True)
