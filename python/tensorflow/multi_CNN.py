ded02c2d19533fcfbdeb7bb86f099463daff3cc4
yuncliu <lyc1990@qq.com>
2016-07-13 05:30:37 -0400

    add writer

--- python/tensorflow/multi_CNN.py
+++ python/tensorflow/multi_CNN.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 import tensorflow as tf
@@ -15,21 +14,22 @@ def bias_variable(shape):
     return tf.Variable(initial)
 
 def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')  
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
 def max_pool_2x2(x):
     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
-x = tf.placeholder(tf.float32, [None, 784])
-y_ = tf.placeholder(tf.float32, [None, 10])
+x = tf.placeholder(tf.float32, [None, 784], name="XInput")
+y_ = tf.placeholder(tf.float32, [None, 10], name="YInput")
 W_conv1 = weight_variable([5, 5, 1, 32])
 b_conv1 = bias_variable([32])
 x_image = tf.reshape(x, [-1,28,28,1])
-h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1, name = "layer1")
 h_pool1 = max_pool_2x2(h_conv1)
 
 W_conv2 = weight_variable([5, 5, 32, 64])
 b_conv2 = bias_variable([64])
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)  
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2, name = "layer2")
 h_pool2 = max_pool_2x2(h_conv2)
 
 W_fc1 = weight_variable([7 * 7 * 64, 1024])
@@ -48,13 +48,20 @@ cross_entropy = -tf.reduce_sum(y_*tf.log(y_conv))
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+writer = tf.train.SummaryWriter("cnn", sess.graph)
+tf.scalar_summary("cross_entropy", cross_entropy)
+merged = tf.merge_all_summaries()
+
 sess.run(tf.initialize_all_variables())
 for i in range(20000):
     batch = mnist.train.next_batch(50)
     if i%100 == 0:
         train_accuracy = accuracy.eval(feed_dict={ x:batch[0], y_: batch[1], keep_prob: 1.0})
         print("step %d, training accuracy %g"%(i, train_accuracy))
+        _, summary = sess.run([train_step, merged], feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+    if i%10 == 0:
+        writer.add_summary(summary, i)
 
-    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
 print("test accuracy %g"%accuracy.eval(feed_dict={ x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
